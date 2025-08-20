3c3b2e9ac877a36f9009f2e3d6f8d5579ed91794
valerianilucrezia <valerianilucrezia11@gmail.com>
2025-08-20 11:45:11 +0200

    add support to Battenberg calls

--- modules/local/cna2CNAqc/templates/main_script.R
+++ modules/local/cna2CNAqc/templates/main_script.R
@@ -41,11 +41,30 @@ parse_ASCAT = function(segments_file, extra_file){
     return(list(segments = segments, purity = purity, ploidy = ploidy))
 }
 
+parse_Battenberg = function(segments_file, extra_file){
+    # Extract the extra information
+    purity_ploidy = read.table(extra_file, sep = '\t', header = T) %>%
+        dplyr::filter(is.best == TRUE) %>%
+        dplyr::rename(purity = rho) %>%
+        dplyr::select(purity, ploidy)
+
+    # Extract the segments information
+    segments = readr::read_tsv(segments_file, col_types = readr::cols()) %>%
+        dplyr::select(chr, startpos, endpos, nMaj1_A, nMin1_A, nMaj2_A, nMin2_A,frac1_A) %>%
+        dplyr::rename(from = startpos, to = endpos) %>%
+        dplyr::rename(Major = nMaj1_A, minor = nMin1_A, Major_2 = nMaj2_A, minor_2 = nMin2_A, CCF = frac1_A)
+
+    return(list(segments = segments, purity = purity_ploidy$purity, ploidy = purity_ploidy$ploidy))
+}
+
 if ("$meta.cna_caller" == 'sequenza'){
 CNA = parse_Sequenza(segments = "$cna_segs", extra = "$cna_extra")
 
 } else if ("$meta.cna_caller" == 'ASCAT'){
 CNA = parse_ASCAT(segments = "$cna_segs", extra = "$cna_extra")
+
+} else if ("$meta.cna_caller" == 'Battenberg'){
+CNA = parse_Battenberg(segments = "$cna_segs", extra = "$cna_extra")
 
 } else {
 stop('Copy Number Caller not supported.')
